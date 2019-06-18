4b418a9418b8c2681c8ff5b53bf3570752417b41
Madison Bentley <mb.madisonbentley@gmail.com>
2019-06-17 23:37:42 -0700

    gave up on hellojs as documentation and info are both too old. Will now try with regular google auth

--- src/oauth.js
+++ src/oauth.js
@@ -1,8 +1,9 @@
-import hello from 'hellojs';
+import * as hello from 'hellojs';
 
 const API_KEY= `94631634763-hnonm7ldimcfbnh9m5t7l5gff4vhsufi.apps.googleusercontent.com`;
 const CLIENT_SECRET=`ofs6XyoDnK_NRm-PdSCbfdA2`;
 
+console.log("hi");
 hello.init({
   google:API_KEY
 })
