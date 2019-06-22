b5908e870976c4d4faa9b554797fe1c8f8891e01
Madison Bentley <mb.madisonbentley@gmail.com>
2019-06-22 11:24:02 -0700

    fixed oauth issues - logging in and out now functional

--- src/oauth.js
+++ src/oauth.js
@@ -3,27 +3,28 @@ import {signIn, signOut} from './actions';
 import {connect} from 'react-redux';
 
 const API_KEY= `94631634763-hnonm7ldimcfbnh9m5t7l5gff4vhsufi.apps.googleusercontent.com`;
-const CLIENT_SECRET=`ofs6XyoDnK_NRm-PdSCbfdA2`;
 
 class Oauth extends React.Component{
 
   componentDidMount(){
     window.gapi.load('client:auth2',()=>{
       window.gapi.client.init({
-        'clientId':API_KEY,
-        'scope':'profile'
+        clientId :API_KEY,
+        scope:'email'
       }).then(() =>{
         this.GoogleAuth = window.gapi.auth2.getAuthInstance();
         this.onAuthChange(this.GoogleAuth.isSignedIn.get());
-        this.GoogleAuth.isSignedIn.listen(this.onAuthChange());
+        this.GoogleAuth.isSignedIn.listen(this.onAuthChange);
       });
     });
   }
   onAuthChange = (isSignedIn) => {
-    if (!isSignedIn){
-      this.props.signIn(this.GoogleAuth.currentUser.Ab.El); //update state
+    if (isSignedIn){
+      console.log(this.GoogleAuth.currentUser.get().getId());
+      this.props.signIn(this.GoogleAuth.currentUser.get().getId()); //update state
     }
     else{
+      console.log("is not signed in");
       this.props.signOut(); //update state
     }
   }
@@ -31,11 +32,15 @@ class Oauth extends React.Component{
     this.GoogleAuth.signIn(); //oauth fn
   }
   onSignOutClick = () =>{
-    console.log("click");
+    console.log(this.GoogleAuth.isSignedIn.get());
     this.GoogleAuth.signOut(); //oauth fn
+    console.log(this.GoogleAuth.isSignedIn.get());
+
   }
   renderButton = () => {
-    if (this.props.signedIn){
+    if (this.props.signedIn == null){
+      return null;
+    }else if(this.props.signedIn){
       return <button onClick={this.onSignOutClick}>Sign Out</button>
     }
     else{
