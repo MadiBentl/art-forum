2824b2b3abb9cdc91d053c600044bb5d36072933
Madison Bentley <mb.madisonbentley@gmail.com>
2019-06-23 22:40:01 -0700

    Add form submission functionality; doesnt work, but doesnt not work either.

--- src/Components/ImageCreate.js
+++ src/Components/ImageCreate.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ImageForm from './ImageForm';
 
 class ImageCreate extends React.Component{
+  submit = values => {
+    console.log(values);
+  }
   render(){
     return(
       <div className="ui container">
         <h1>Add your art!</h1>
         <div className="ui form">
-          <ImageForm />
+          <ImageForm onSubmit={this.submit}/>
         </div>
       </div>)
   }
