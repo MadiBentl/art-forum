402c37ab20c52284f5071062e2e5e5091232f0aa
Madison Bentley <mb.madisonbentley@gmail.com>
2019-07-14 18:32:22 -0700

    imported state data + necessary actions into edit component

--- src/Components/ImageEdit.js
+++ src/Components/ImageEdit.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ImageForm from './ImageForm';
+import {connect} from 'react-redux';
+import {fetchArt} from '../actions';
 
 class ImageEdit extends React.Component{
+  componentDidMount(){
+    this.imageId = this.props.match.params.imageId;
+    this.props.fetchArt(this.imageId);
+  }
   render(){
-    return(<div>
+    return(<div className="ui form">
         <ImageForm onSubmit={() => console.log("submit")}/>
       </div>)
   }
 }
+const mapStateToProps = (state) =>{
+  return{
+    images:state.images
+  }
+}
 
-export default ImageEdit;
+export default connect(mapStateToProps, {fetchArt})(ImageEdit);
